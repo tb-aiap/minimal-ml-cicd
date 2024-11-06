b6dae4699d9b2ab3a320317fc4ea22dd0ec7319b
tb-aiap <towboon@gmail.com>
2024-11-06 17:31:09 +0800

    feat: Initial commit for api retriever.

---
+++ src/ml/retrieve_data.py
@@ -0,0 +1,55 @@
+"""Module to retrieve the data from API."""
+
+import logging
+import os
+from typing import Any
+
+import dotenv
+import requests
+
+dotenv.load_dotenv()
+
+logger = logging.getLogger(__name__)
+
+BASE_URL = os.getenv("BASE_URL")
+API_URL = os.getenv("API_URL")
+
+if API_URL is None:
+    error_msg = "API_URL is None, check environment variable"
+    logger.error(error_msg)
+    raise ValueError(error_msg)
+
+
+def get_single_response(api_url: str):
+    """Get a single api call response"""
+    response = requests.get(api_url)
+
+    return response
+
+
+def get_multiple_offset_response(entry_number: int = 500) -> list[dict[str, Any]]:
+
+    response = get_single_response(API_URL)
+
+    resp = response.json()
+    resp = resp["result"]
+
+    hdb_results = resp["records"]
+    total_records = resp["total"] if entry_number == 0 else entry_number
+    offset_records = resp.get("offset", 0)
+
+    while offset_records < total_records:
+
+        logger.debug("Retrieving additional records", offset_records, total_records)
+
+        next_api = BASE_URL + resp["_links"]["next"]
+
+        response = get_single_response(next_api)
+
+        resp = response.json()
+        resp = resp["result"]
+        offset_records = resp.get("offset")
+
+        hdb_results.extend(resp["records"])
+
+    return hdb_results
