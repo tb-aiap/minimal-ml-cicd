d411433fe8d5b7a4bbdacc0589353b049c110023
tb-aiap <towboon@gmail.com>
2025-01-22 13:30:18 +0800

    feat: Added feature engineered columns.

--- src/train_model/data_model.py
+++ src/train_model/data_model.py
@@ -15,10 +15,16 @@ class ColumnEnum(str, Enum):
     flat_model = "flat_model"
     lease_commence_date = "lease_commence_date"
     storey_range = "storey_range"
+    floor_area_sqm = "floor_area_sqm"
+    remaining_lease = "remaining_lease"
 
     # cleaned column
     storey_from = "storey_from"
     storey_to = "storey_to"
+
+    # feature engineered column
+    storey_area_ratio = "storey_area_ratio"
+    lease_less_than_50_yrs = "lease_less_than_50_yrs"
 
 
 class HDBData(BaseModel):
