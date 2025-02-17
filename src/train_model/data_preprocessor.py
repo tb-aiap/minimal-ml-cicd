61737f5423697fbcf979688ee625bde0793532d4
tb-aiap <towboon@gmail.com>
2025-02-17 19:30:03 +0800

    feat: Updated preprocessor with process_data and transform_data functions.

--- src/train_model/data_preprocessor.py
+++ src/train_model/data_preprocessor.py
@@ -2,12 +2,15 @@
 
 import logging
 from abc import ABC, abstractmethod
+from pathlib import Path
 
+import numpy as np
 import omegaconf
 import pandas as pd
+import scipy.sparse
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
-from . import data_model
+from . import data_model, utils
 
 logger = logging.getLogger(__name__)
 COL = data_model.ColumnEnum
@@ -20,7 +23,7 @@ class DataPreprocessor(ABC):
     """For cleaning data into required format."""
 
     @abstractmethod
-    def preprocess_data(self, data: pd.DataFrame):
+    def fit_preprocessors(self, data: pd.DataFrame):
         """Main abstract method to clean data."""
         ...
 
@@ -33,31 +36,89 @@ class DataPreprocessor(ABC):
 class HdbDataPreprocessor(DataPreprocessor):
     """For feature engineering and additional preprocessing."""
 
-    def __init__(self, params: omegaconf.DictConfig):
+    def __init__(
+        self, params: omegaconf.DictConfig, object_filepath: str | Path
+    ) -> None:
         """Initialize data preprocessor."""
         self.params = params
+        self.object_filepath = object_filepath
+        self._preprocessors = []
         self._validate_params()
 
     def _validate_params(self):
         """Check the following in params.
 
-        If preprocessors are typed correctly in config file.
+        - If preprocessors are typed correctly in config file.
         """
         for key in self.params:
             if PREPROCESSOR.get(key) is None:
-                (self.params)
                 raise NotImplementedError(
                     f"{key} is either not implemented or check spelling."
                 )
 
-    def preprocess_data(self, data: pd.DataFrame) -> pd.DataFrame:
-        """Placeholder for preprocessing / scaling ."""
+        if isinstance(self.object_filepath, str):
+            logger.debug(f"Converting {self.object_filepath} in to Path object.")
+            self.object_filepath = Path(self.object_filepath)
+
+    def fit_preprocessors(self, data: pd.DataFrame) -> pd.DataFrame:
+        """Placeholder for preprocessing / scaling .
+
+        Save the preprocessor after fit_transform.
+        Load the preprocess for transform.
+        """
         for key in self.params:
             preprocessor = PREPROCESSOR.get(key)()
             preprocess_data = data[self.params[key]["columns"]]
-            scaled_data = preprocessor.fit_transform(preprocess_data)
+            preprocessor.fit(preprocess_data)
+            self._preprocessors.append(preprocessor)
 
-        return scaled_data
+        self._save_preprocessors()
+        return None
+
+    def _save_preprocessors(self):
+        """Save preprocessors after fitting it."""
+        for p in self._preprocessors:
+            logger.debug(f"Saving preprocessor {p} into {self.object_filepath} folder.")
+            p_name = p.__class__.__name__.lower()
+            p_file_path = Path(self.object_filepath, p_name)
+            utils.utils.save_object(p, p_file_path)
+
+    def transform_data(self, data: pd.DataFrame) -> pd.DataFrame:
+        """Uses fitted scaler to preprocessing / scaling .
+
+        Load the preprocess for transform if self._preprocessors is empty.
+        Uses exisiting self.preprocessors after fitted for transforming data.
+        """
+        scaled_data_array = []
+        scaled_data_names = []
+
+        if not self._preprocessors:
+            logger.info(f"Preprocessor attribute is empty {self._preprocessors=}.")
+            logger.info(f"Loading preprocesors from {self.object_filepath}")
+
+            for key in self.params:
+                p_file_path = Path(self.object_filepath, key + ".pkl")
+                preprocessor = utils.utils.load_object(p_file_path)
+                self._preprocessors.append(preprocessor)
+
+        for p in self._preprocessors:
+            p_name = p.__class__.__name__.lower()
+            logger.info(f"Transforming data with {p_name}")
+            preprocess_data = data[self.params[p_name]["columns"]]
+            scaled_data = p.transform(preprocess_data)
+
+            if scipy.sparse.issparse(scaled_data):
+                logger.debug(f"Converting sparse matrix from {p}")
+                scaled_data = scaled_data.toarray()
+
+            scaled_data_array.append(scaled_data)
+            scaled_data_names.append(p.get_feature_names_out())
+
+        df = pd.DataFrame(
+            np.concatenate(scaled_data_array, axis=1),
+            columns=np.concatenate(scaled_data_names),
+        )
+        return df
 
     def feature_engineer(self, data: pd.DataFrame) -> pd.DataFrame:
         """Main function to feature engineer data."""
--- src/train_model/tests/test_data_preprocessor.py
+++ src/train_model/tests/test_data_preprocessor.py
@@ -1,5 +1,7 @@
 """Test module for data preprocessor."""
 
+from pathlib import Path
+
 import omegaconf
 import pandas as pd
 import pytest
@@ -10,13 +12,14 @@ COL = tm.data_model.ColumnEnum
 
 
 @pytest.fixture
-def config():
+def config(tmp_path):
     """Config yaml mock for preprocessor."""
     config = {
+        "save_path": tmp_path,
         "preprocessor": {
             "standardscaler": {"columns": ["floor_area_sqm", "storey_to"]},
             "onehotencoder": {"columns": ["Column2", "Column3"]},
-        }
+        },
     }
     return omegaconf.DictConfig(config)
 
@@ -39,21 +42,78 @@ def data():
 
 def test_preprocessor_init(config):
     """Test preprocessor config initialization."""
-    preprocessor = tm.data_preprocessor.HdbDataPreprocessor(config.preprocessor)
+    preprocessor = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
     assert "standardscaler" in preprocessor.params
     assert "onehotencoder" in preprocessor.params
 
 
 def test_preprocessor_preprocess(config, data):
     """Test preprocess main method."""
-    preprocessor = tm.data_preprocessor.HdbDataPreprocessor(config.preprocessor)
-    scaled_data = preprocessor.preprocess_data(data)
-    print(scaled_data)
+    preprocessor = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
+    preprocessor.fit_preprocessors(data)
+    assert [i.__class__.__name__ for i in preprocessor._preprocessors] == [
+        "StandardScaler",
+        "OneHotEncoder",
+    ]
+    preprocessor_2 = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
+    assert preprocessor_2._preprocessors == []
+
+
+def test_preprocessor_save_scalers_properly(config, data):
+    """Test preprocessor saves scalers into provided folder."""
+    preprocessor = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
+    assert list(Path(config.save_path).iterdir()) == []
+    preprocessor.fit_preprocessors(data)
+    assert list(Path(config.save_path).iterdir()) != []
+
+
+def test_preprocessor_transform_data(config, data):
+    """Test preprocess transforms data into dataframe."""
+    preprocessor = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
+    preprocessor.fit_preprocessors(data)
+    assert [i.__class__.__name__ for i in preprocessor._preprocessors] == [
+        "StandardScaler",
+        "OneHotEncoder",
+    ]
+    dataframe = preprocessor.transform_data(data)
+
+    assert isinstance(dataframe, pd.DataFrame)
+    assert dataframe.shape[0] == 5
+    assert dataframe.shape[1] == 9
+
+
+def test_preprocessor_transform_data_inference(config, data):
+    """Test preprocess main method."""
+    preprocessor = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
+    preprocessor.fit_preprocessors(data)
+
+    preprocessor_2 = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
+    scaled_df = preprocessor_2.transform_data(data)
+
+    assert isinstance(scaled_df, pd.DataFrame)
+    assert scaled_df.shape[0] == 5
+    assert scaled_df.shape[1] == 9
 
 
 def test_feature_engineer(config, data):
     """Test main feature engineer function."""
-    cleaner = tm.data_preprocessor.HdbDataPreprocessor(config.preprocessor)
+    cleaner = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
     cleaned_data = cleaner.feature_engineer(data)
 
     assert COL.storey_area_ratio in cleaned_data.columns
@@ -62,7 +122,9 @@ def test_feature_engineer(config, data):
 
 def test_fe_ratio_storey_to_floor_area(config, data):
     """Test feature engineer storey range."""
-    cleaner = tm.data_preprocessor.HdbDataPreprocessor(config.preprocessor)
+    cleaner = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
     cleaned_data = cleaner._fe_ratio_storey_to_floor_area(data)
 
     assert COL.storey_area_ratio in cleaned_data.columns
@@ -72,7 +134,9 @@ def test_fe_ratio_storey_to_floor_area(config, data):
 
 def test_fe_lease_less_than_50_yrs(config, data):
     """Test feature engineer for lease less than 50 yrs bool."""
-    cleaner = tm.data_preprocessor.HdbDataPreprocessor(config.preprocessor)
+    cleaner = tm.data_preprocessor.HdbDataPreprocessor(
+        config.preprocessor, config.save_path
+    )
     cleaned_data = cleaner._fe_lease_less_than_50_yrs(data)
 
     assert COL.lease_less_than_50_yrs in cleaned_data.columns
