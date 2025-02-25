a1b57d94eec9b382acc365b14dafa6d1610b7df2
tb-aiap <towboon@gmail.com>
2025-02-25 13:38:38 +0800

    feat: Updated evaluator class with relevant tests and CVMetric class to store cv results.

--- src/train_model/evaluator.py
+++ src/train_model/evaluator.py
@@ -1,7 +1,10 @@
 """Module to evaluate the results of the a model prediction."""
 
 import logging
+from collections import defaultdict
+from typing import Any, Callable
 
+import numpy as np
 import omegaconf
 
 import train_model.utils.utils as utils
@@ -24,7 +27,7 @@ class Evaluator:
     str "sklearn.metrics.f1_score" becomes function `f1_score()`.
     """
 
-    def __init__(self, params: omegaconf.DictConfig):
+    def __init__(self, params: omegaconf.DictConfig) -> None:
         """Initialize evaluator with required params.
 
         Params required is a list of metrics to measure for model performance.
@@ -32,18 +35,41 @@ class Evaluator:
 
         """
         self.params: list[str] = params
-        self.metrics: dict[str, float] = dict()
 
-    def __validate_params(self) -> None: ...
-
-    def initialize_metrics_fn(self):
+    def initialize_metrics_fn(self) -> list[Callable[[Any, Any], float]]:
         """Reads a list of str and parse it into Python functions."""
         return [utils.load_func(p) for p in self.params]
 
     def evaluate(self, ypred, ytrue):
         """Evaluates ypred and ytrue with a list of metrics provided in params."""
         metrics_fn = self.initialize_metrics_fn()
+        self.metrics: dict[str, float] = dict()
+
         for fn in metrics_fn:
             fn_name = fn.__name__
             fn_result = fn(ytrue, ypred)
             self.metrics[fn_name] = fn_result
+
+
+class CVMetrics:
+    """Data class to store evaluation metrics from cross validation(cv)."""
+
+    def __init__(self):
+        """Initialize class with metrics attribute to store cv results."""
+        self.metrics = defaultdict(list)
+
+    def update_metrics(self, current_metrics: dict[str, float]) -> None:
+        """Ingest current training metrics and store it across cv."""
+        for k, v in current_metrics.items():
+            self.metrics[k].append(v)
+
+    def get_mean(self, metric_name: str) -> float:
+        """Get the mean metrics of selected metrics across all cv."""
+        result = self.metrics.get(metric_name)
+        if result is None:
+            raise KeyError(
+                f"Selected Metric {metric_name} is not a valid name.",
+                f"Expected the followings from {list(self.metrics.keys())}",
+            )
+
+        return np.mean(result)
